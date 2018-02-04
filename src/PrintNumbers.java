90278b5921e24e4fe99266e4b08bd9d924b7b472
AmiPiyush <amipiyush@gmail.com>
2018-02-04 22:07:51 +0000

    one line added

--- src/PrintNumbers.java
+++ src/PrintNumbers.java
@@ -1,6 +1,7 @@
 public class PrintNumbers {
 
         public static void main (String args[]){
+            System.out.println("0");
             System.out.println("1");
             System.out.println("2");
             System.out.println("3");
